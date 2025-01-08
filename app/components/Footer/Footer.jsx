f28c77dcbd92174f94fb0275db01633e47ba7c95
Brahmahamadou <mahamadoubrah672@gmail.com>
2025-01-09 00:29:18 +0100

    correction footrt

--- app/components/Footer/Footer.jsx
+++ app/components/Footer/Footer.jsx
@@ -1,9 +1,21 @@
 import { FaFacebook, FaTwitter, FaLinkedin } from 'react-icons/fa'; // Importer les icônes
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4 text-center">
-      <p>© 2024 Niger Fact-Check. Tous droits réservés.</p>
+      <div className='flex justify-around items-center'>
+      <div className="">
+        <Link href="/">
+          <Image
+            src="/images/fact2.png" // Remplacez par le chemin de votre logo
+            alt="Logo"
+            width={120}
+            height={120}
+          />
+        </Link>
+      </div>
       <div className="flex justify-center space-x-6">
         <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
           <FaFacebook size={30} className="hover:text-blue-600" />
@@ -15,6 +27,9 @@ export default function Footer() {
           <FaLinkedin size={30} className="hover:text-blue-700" />
         </a>
       </div>
+      </div>
+      <p>© 2024 Niger Fact-Check. Tous droits réservés.</p>
+  
     </footer>
   );
 }
